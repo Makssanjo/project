2a608e24d0a6b15c19136301cf57477b52f1bb7d
Maks <makssan@meta.ua>
2022-11-07 01:47:46 +0200

    first test commits

---
+++ JSCode/JS/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//   console.log(obj["Petro"]);
+// const result = confirm("Вам есть 18?");
+// console.log(result);
+
+// const pomep = prompt("Cколько вам лет?","");
+// console.log(pomep);
+
+// const answers = [];
+// answers[0] = prompt("Как ваше имя?",'');
+// answers[1] = prompt("Cколько вам лет?",'');
+// answers[2] = prompt("Как ваша фамилия?",'');
+
+// document.write(answers);
+// let incr = 15,
+//     decr = 14;
+// incr++;
+// decr--;
+
+//     console.log(incr);
+//     console.log(decr);
+// console.log(5**4);
+
+// const categories = 'gemini';
+// console.log(`http//somesait.com/${categories}/4/tumir/gtur`);
